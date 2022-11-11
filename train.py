73959528d2e4f4b55d3196401eadd15568964f5f
barak.bendayan <barak.bendayan@zeekit.me>
2022-11-11 12:33:22 +0200

    changelog:     - step size now controlled from terminal

--- train.py
+++ train.py
@@ -8,7 +8,7 @@ upro_df = pd.read_csv('./data/UPROSIM_preprocessed.csv')
 tmf_df = pd.read_csv('./data/TMFSIM_preprocessed.csv')
 
 
-def train(to_index=6714, checkpoint_name='ppo_portfolio'):
+def train(to_index=6714, checkpoint_name='ppo_portfolio', step_size=70):
     env = make_vec_env(PortfolioEnv, env_kwargs={'upro_df': upro_df[:to_index], 'tmf_df': tmf_df[:to_index]}, n_envs=8,
                        monitor_dir='./logs')
     model = PPO('MlpPolicy', env, verbose=1, learning_rate=1e-4, batch_size=256, policy_kwargs={'net_arch': [dict(pi=[256, 256, 256, 256], vf=[256, 256, 256, 256])]})
@@ -16,7 +16,7 @@ def train(to_index=6714, checkpoint_name='ppo_portfolio'):
     model.save(checkpoint_name)
 
 
-def infer(from_index=6714, checkpoint_name='ppo_portfolio'):
+def infer(from_index=6714, checkpoint_name='ppo_portfolio', step_size=70):
     model = PPO.load(checkpoint_name)
     env = PortfolioEnv(upro_df=upro_df[from_index:], tmf_df=tmf_df[from_index:])
     obs = env.reset()
@@ -39,11 +39,12 @@ def infer(from_index=6714, checkpoint_name='ppo_portfolio'):
 @click.option('-i', '--isinfer', is_flag=True, default=False, help='Infer')
 @click.option('-n', '--checkpoint_name', type=str, default='ppo_portfolio', help='checkpoint name')
 @click.option('--split', type=int, default=6714, help='Index to split train and test data')
-def main(istrain=False, isinfer=False, checkpoint_name='ppo_portfolio', split=6714):
+@click.option('-s', '--step-size', type=int, default=70, help='step size of the environment')
+def main(istrain=False, isinfer=False, checkpoint_name='ppo_portfolio', split=6714, step_size=70):
     if istrain:
-        train(to_index=split, checkpoint_name=checkpoint_name)
+        train(to_index=split, checkpoint_name=checkpoint_name, step_size=step_size)
     if isinfer:
-        infer(from_index=split, checkpoint_name=checkpoint_name)
+        infer(from_index=split, checkpoint_name=checkpoint_name, step_size=step_size)
 
 
 if __name__ == '__main__':
